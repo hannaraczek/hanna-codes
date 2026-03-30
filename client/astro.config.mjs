702dca58a84aa6f9091343c9b61bc470f7d57c0c
Hanna Raczek <hanna.raczek@gmail.com>
2026-03-30 00:41:33 -0500

    revert confiog

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -4,20 +4,17 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 import sanity from 'astro-sanity';
+
 import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-  site: 'https://hanna.codes',
-  integrations: [sitemap(), sanity({
-    projectId: 'uh7kbf9e',
-    dataset: 'production',
-    apiVersion: '2026-03-20',
-    useCdn: true,
-    studio: {
-      basePath: '/studio',
-      config: '../cms/sanity.config.ts'
-    }
-  }), solidJs()]
+    adapter: netlify(),
+    site: 'https://hanna.codes',
+    integrations: [sitemap(), sanity({
+        projectId: 'uh7kbf9e',
+        dataset: 'production',
+        apiVersion: '2026-03-20',
+        useCdn: true
+    }), solidJs()]
 });
